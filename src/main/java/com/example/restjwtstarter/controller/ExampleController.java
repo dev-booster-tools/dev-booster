bd934f486161215e572a1aff54a829b756f008ac
Yuriy Titov <yurtitov@gmail.com>
2023-09-27 12:35:15 +0200

    Add simple controller

---
+++ src/main/java/com/example/restjwtstarter/controller/ExampleController.java
@@ -0,0 +1,24 @@
+package com.example.restjwtstarter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ExampleController.ROOT_PATH)
+public class ExampleController {
+
+    public static final String ROOT_PATH = "api";
+    private static final String USER_PATH = "user";
+    private static final String ADMIN_PATH = "admin";
+
+    @GetMapping(USER_PATH)
+    public String helloUser() {
+        return "Hello User";
+    }
+
+    @GetMapping(ADMIN_PATH)
+    public String helloAdmin() {
+        return "Hello Admin";
+    }
+}
