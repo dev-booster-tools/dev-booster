95a603824b0fd850a680c54bb32fdbee7642b196
Yuriy Titov <yurtitov@gmail.com>
2023-09-27 12:27:09 +0200

    Init commit

---
+++ src/main/java/com/example/restjwtstarter/RestJwtStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.restjwtstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJwtStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestJwtStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restjwtstarter/RestJwtStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restjwtstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJwtStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
