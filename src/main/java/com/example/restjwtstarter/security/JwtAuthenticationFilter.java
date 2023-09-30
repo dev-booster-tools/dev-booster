5714e91da56d1b7d726b452f43722c3663a6c141
Yuriy Titov <yurtitov@gmail.com>
2023-09-30 19:11:45 +0200

    Add ability to use JWT in requests

---
+++ src/main/java/com/example/restjwtstarter/security/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.example.restjwtstarter.security;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtDecoder jwtDecoder;
+    private final JwtToPrincipalConverter jwtToPrincipalConverter;
+
+    @Override
+    protected void doFilterInternal(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        extractTokenFromRequest(request)
+                .map(jwtDecoder::decode)
+                .map(jwtToPrincipalConverter::convert)
+                .map(UserPrincipalAuthenticationToken::new)
+                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> extractTokenFromRequest(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            return Optional.of(token.substring(7));
+        }
+
+        return Optional.empty();
+    }
+}
